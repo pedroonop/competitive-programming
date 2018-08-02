cccba7abae26f5f34359470698d7657f75285dd1
enoque_alves <enoque_itb@outlook.com>
2018-08-01 21:08:33 -0300

    codigo LCA

--- algoritmos/LCA(With_RMQ).cpp
+++ algoritmos/LCA(With_RMQ).cpp
@@ -4,6 +4,7 @@
 #define MAXV 100
 #define mid(l, r) 	       ((l + r) >> 1)
 #define INF 0x3f3f3f3f
+#define LOG 25
 using namespace std;
 vector <vi> g;
 vi euler;
@@ -12,8 +13,64 @@ int first[MAXV];
 ii tree[8*MAXV];
 ii sqrtDeco[8*MAXV];
 
+int potencia2[LOG];
+int dp[MAXV][LOG];
+int logn[MAXV];
 
-void build(int node, int left ,int right){
+
+
+
+void buildSparseTable(int n){   //O(NlogN) para construir
+    // initializing sparse table
+    memset(dp,-1,sizeof(dp));
+    // filling base case values
+    for (int i=1; i<n; i++)
+        dp[i-1][0] = (altura[i]>altura[i-1]) ? i-1 : i;
+ 
+    // dp to fill sparse table
+    for (int l=1; l<15; l++)
+      for (int i=0; i<n; i++)
+        if (dp[i][l-1]!=-1 and dp[i+potencia2[l-1]][l-1]!=-1)
+          dp[i][l] = (altura[dp[i][l-1]] > altura[dp[i+potencia2[l-1]][l-1]]) ? dp[i+potencia2[l-1]][l-1] : dp[i][l-1];
+        else
+             break;
+}
+ 
+int query_sparse_table(int l,int r){ //O(1) para buscar
+    int d = r-l;
+    int dx = logn[d];
+    if (l==r) return l;
+    if (altura[dp[l][dx]] > altura[dp[r-potencia2[dx]][dx]])
+        return dp[r-potencia2[dx]][dx];
+    else
+        return dp[l][dx];
+}
+ 
+void preprocess(){  //O(N) para preprocessar
+    potencia2[0] = 1;
+    for (int i=1; i<18; i++)
+        potencia2[i] = potencia2[i-1]*2;
+ 
+    int val = 1,ptr=0;
+    for (int i=1; i < MAXV; i++){
+        logn[i] = ptr-1;
+        if (val==i){
+            val*=2;
+            logn[i] = ptr;
+			ptr++;
+        }
+    }
+}
+
+
+int LCA_sparse_table (int u, int v){ //O(1) para achar o LCA
+	if (u == v) return u;
+	if (first[u] > first[v]) swap(u,v);
+	return euler[query_sparse_table(first[u], first[v])];
+}
+
+
+void build(int node, int left ,int right){ // O(N) para criar a segtree
 	if(left == right){
 		tree[node] = ii(altura[left], euler[left]);
 		return;
@@ -27,7 +84,7 @@ void build(int node, int left ,int right){
 	else tree[node] = tree[dir];
 }
 
-ii query(int node,int left ,int right ,int lq , int rq){
+ii query(int node,int left ,int right ,int lq , int rq){ //O(logN) para buscar na segtree
 	if(right < lq || left > rq)
 		return ii(INF,-1);
 		
@@ -42,7 +99,7 @@ ii query(int node,int left ,int right ,int lq , int rq){
 }
 
 
-void build_sqrt(){
+void build_sqrt(){ //O(N) para criar a sqrt decomposition
 	int block = sqrt(altura.size());
 	for (int i = 0; i < block; i++) sqrtDeco[i] = ii(INF, -1);
 	for (int i = 0; i < (int) altura.size(); i++){
@@ -52,7 +109,7 @@ void build_sqrt(){
 }
 
 
-ii query_sqrt(int l, int r){
+ii query_sqrt(int l, int r){ //O(sqrt(N) para buscar
 	int block = sqrt(altura.size());
 	ii ans = ii(INF, -1);
 	while (l % block != 0 && l < r){
@@ -87,13 +144,13 @@ void dfs (int v, int nivel){
 	}
 }
 
-ii LCA_segtree (int v, int u){
+ii LCA_segtree (int v, int u){ // O(logN)
 	int l = first[v];
 	int r = first[u];
 	if (l > r) swap (l,r);
 	return query (0, 0, altura.size()-1, l, r);
 }
-ii LCA_sqrt (int v, int u){
+ii LCA_sqrt (int v, int u){ //O(sqrt(N))
 	int l = first[v];
 	int r = first[u];
 	if (l > r) swap (l,r);
@@ -120,13 +177,24 @@ int main (){
 	printf ("\n");
 	build (0, 0, altura.size()-1);
 	build_sqrt ();
+	preprocess();
+	buildSparseTable (altura.size());
 	
 	printf ("LCA_segtree (1,2) = %d\n", LCA_segtree(1,2).second);
 	printf ("LCA_sqrt (1,2) = %d\n", LCA_sqrt(1,2).second);
+	printf ("LCA_sparse (1,2) = %d\n", LCA_sparse_table(1,2));
 	printf ("LCA_segtree (4,5) = %d\n", LCA_segtree(4,5).second);
 	printf ("LCA_sqrt (4,5) = %d\n", LCA_sqrt(4,5).second);
+	printf ("LCA_sparse (4,5) = %d\n", LCA_sparse_table(4,5));
 	printf ("LCA_segtree (4,3) = %d\n", LCA_segtree(4,3).second);
 	printf ("LCA_sqrt (4,3) = %d\n", LCA_sqrt(4,3).second);
+	printf ("LCA_sparse (4,3) = %d\n", LCA_sparse_table(4,3));
+	printf ("LCA_segtree (6,7) = %d\n", LCA_segtree(6,7).second);
+	printf ("LCA_sqrt (6,7) = %d\n", LCA_sqrt(6,7).second);
+	printf ("LCA_sparse (6,7) = %d\n", LCA_sparse_table(6,7));
+	printf ("LCA_segtree (9,12) = %d\n", LCA_segtree(9,12).second);
+	printf ("LCA_sqrt (9,12) = %d\n", LCA_sqrt(9,12).second);
+	printf ("LCA_sparse (9,12) = %d\n", LCA_sparse_table(9,12));
 	//printf ("LCA (6,4) = %d\n", euler[LCA_segtree(6,4).second]);
 	//printf ("LCA (5,7) = %d\n", euler[LCA_segtree(5,7).second]);
 	return 0;
