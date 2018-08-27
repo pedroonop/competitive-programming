84ec3dffa01e7f186e429b375a03e70de1ea487f
Pedro Torres <cs.pedrotorres@gmail.com>
2018-08-26 22:37:40 -0300

    kmp do enoque

--- contests/iv_maratona_mineira/j.cpp
+++ contests/iv_maratona_mineira/j.cpp
@@ -9,6 +9,7 @@ using namespace std;
 int b[MAX_N], n, m; // b = back table, n = length of T, m = length of P
 
 void kmpPreprocess(string P) { // call this before calling kmpSearch()
+	m = P.size();
   int i = 0, j = -1; b[0] = -1; // starting values
   while (i < m) { // pre-process the pattern string P
     while (j >= 0 && P[i] != P[j]) j = b[j]; // if different, reset j using b
@@ -18,10 +19,12 @@ void kmpPreprocess(string P) { // call this before calling kmpSearch()
 
 bool kmpSearch(string T, string P) { // this is similar as kmpPreprocess(), but on string T
 	int i = 0, j = 0; // starting values
+	n = T.size(); m = P.size();
 	while (i < n) { // search through string T
-	    while (j >= 0 && T[i] != P[j]) j = b[j]; // if different, reset j using b
+	    while (j >= 0 && T[i] != P[j]){printf("T = %c   P = %c j = %d\n", T[i], P[j], j); j = b[j]; } // if different, reset j using b
 	    i++; j++; // if same, advance both pointers
 	    if (j == m) { // a match found when j == m
+	    	printf("j = %d\n",j );
 	      printf("P is found at index %d in T\n", i - j);
 	      j = b[j]; // prepare j for the next possible match
 	      return true;
@@ -30,36 +33,42 @@ bool kmpSearch(string T, string P) { // this is similar as kmpPreprocess(), but 
 }
 
 int main(){
-  int n;
+  int x;
   string origem, aux, bons, ruins, linha;
-  cin >> origem;
-  cin >> n;
+  cin >> aux;
+  origem = '$';
+  origem += aux;
+  cin >> x;
   go(i,origem.size()){
   	if (origem[i] >= 'A' && origem[i] <= 'Z') origem[i] = origem[i] - 'A'+'a';
   }
-  go(i,n){
+  bons = '$';
+  ruins = '$';
+  go(i,x){
   	cin >> aux;
   	go(i,aux.size()){
   		if (aux[i] >= 'A' && aux[i] <= 'Z') aux[i] = aux[i] - 'A'+'a';
   	}
   	bons += aux;
-  	bons += "#";
+  	//bons += "#";
   }
-  cin >> n;
-  go(i,n){
+  cin >> x;
+  go(i,x){
   	cin >> aux;
   	go(i,aux.size()){
   		if (aux[i] >= 'A' && aux[i] <= 'Z') aux[i] = aux[i] - 'A'+'a';
   	}
   	ruins += aux;
-  	ruins += "#";
+  	//ruins += "#";
   }
-  cin >> n;
+  cin >> x;
   int mais = 0;
   int menos = 0;
   int neutro = 0;
+  cout << origem << endl;
   cout << bons << endl;
-  go(i,n){
+  cout << ruins << endl;
+  go(i,x){
   	getline(cin,linha);
   	getline(cin,linha);
   	bool pode = false;
@@ -72,12 +81,18 @@ int main(){
   			aux += c;
   		}
   		else{
-  			cout << aux << endl;
+    		cout << aux << endl;
   			memset(b, 0, sizeof(b));
   			kmpPreprocess(aux);
-  			if (kmpSearch (origem, aux)) pode = true;
-  			if (kmpSearch(bons, aux)) positivo++;
-  			if (kmpSearch(ruins, aux)) negativo++;
+  			m = aux.size();
+  			n = origem.size();
+  			if (kmpSearch (origem, aux)){ pode = true; cout << aux << ":  origem\n";}
+  			kmpPreprocess(aux);
+  			n = bons.size();
+  			if (kmpSearch(bons, aux)){ positivo++;cout << aux << ":  positivo\n";}
+  			kmpPreprocess(aux);
+  			n = ruins.size();
+  			if (kmpSearch(ruins, aux)){ negativo++; cout << aux << ":  negativo\n";}
   			aux = "";
   		}
   	}
