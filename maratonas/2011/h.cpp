e5cbf02c03a9a6897bf7db0a05d55bafccf1c6ff
Pedro Torres <cs.pedrotorres@gmail.com>
2018-09-06 09:36:50 -0300

    adicionado algoritmos pontos_de_articulacao e pontes

--- maratonas/2011/h.cpp
+++ maratonas/2011/h.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#define go(i,n) for (int i = 0; i < (int)n; i++)
 
 #define mid(l, r) 	       ((l + r) >> 1)
 
@@ -13,14 +14,12 @@ typedef struct{
 const int maxn  = 100000;
 Node tree[4*maxn];
 int lazy[4*maxn];
-int arr[maxn];
-
-
+Node vazio;
 
 
 void build(int node, int left ,int right){
 	if(left == right){
-		tree[node].v[1]++;
+		tree[node].v[0]++;
 		//lazy[node] = 0;
 		return;
 	}
@@ -41,8 +40,12 @@ void update_range( int node, int left, int right, int lq, int rq, int k){
   
   if( lazy[node] ){
   		int v[3];
+  		memset (v,0,sizeof(v));
   		go(i,3){
   			v[i] = tree[node].v[(i+lazy[node])%3];
+  		}
+  		go(i,3){
+  			tree[node].v[i] = v[i];
   		}
 		//tree[node] = (right-left+1)*lazy[node];
 		
@@ -60,9 +63,13 @@ void update_range( int node, int left, int right, int lq, int rq, int k){
   if(left >= lq && right <= rq){ // se o intervalo [lq,rq] cobre totalmente [left, right]
 		
 		//tree[node] = (right-left+1)*k;
+  		int v[3];
+  		memset (v,0,sizeof(v));
   		go(i,3){
-  			tree[node].v[i] += k;
-  			tree[node].v[i] %= 3;
+  			v[i] = tree[node].v[(i+k)%3];
+  		}
+  		go(i,3){
+  			tree[node].v[i] = v[i];
   		}
 		//printf("[%d - %d] = %d\n", left+1, right+1, tree[node]);
 		if( left != right ){ // tem filhos
@@ -88,63 +95,89 @@ void update_range( int node, int left, int right, int lq, int rq, int k){
 }
 
 
-int query(int node,int left ,int right ,int lq , int rq){
+Node query(int node,int left ,int right ,int lq , int rq){
 	
 	if( lazy[node] ){
-		tree[node] = (right-left+1)*lazy[node];
-		
+		//tree[node] = (right-left+1)*lazy[node];
+		int v[3];
+		memset (v,0,sizeof(v));
+  		go(i,3){
+  			v[i] = tree[node].v[(i+lazy[node])%3];
+  		}
+  		go(i,3){
+  			tree[node].v[i] = v[i];
+  		}
 		if( left != right ){ // tem filhos
-			lazy[2*node+1] = lazy[node];
-			lazy[2*node+2] = lazy[node];
+			lazy[2*node+1] += lazy[node];
+			lazy[2*node+2] += lazy[node];
 		}
 		
 		lazy[node] = 0;
 	}
   
 	if(right < lq || left > rq)
-		return 0;
+		return vazio;
 		
 	if(left >= lq && right <= rq)
 		return tree[node];
 	
 	int mid = mid(left,right);
-	int p1 = query(2*node+1,left,mid,lq,rq); 
-	int p2 = query(2*node+2,mid+1,right,lq,rq);
-	
-	return p1 + p2;
+	Node p1 = query(2*node+1,left,mid,lq,rq); 
+	Node p2 = query(2*node+2,mid+1,right,lq,rq);
+	Node p3;
+
+	go(i,3){
+		p3.v[i] = p1.v[i] + p2.v[i];
+	}
+
+	return p3;
 }
 
 
 
 int main(){
+	memset (vazio.v, 0, sizeof(vazio.v));
 	int n, q;
-	int o;
+	char op;
 	int a, b, k;
 	
 	//cin >> n >> q;
-	scanf ("%d %d", &n, &q);
-
-
-	for(int i = 0; i < n; i++) cin >> arr[i];
-		
-	build(0, 0, n-1);
-		
-	for(int i = 1; i <= q; i++){
-		
-		scanf("%d", &o);	
-		
-		if( o == 1){
-			scanf("%d %d %d", &a, &b , &k);
+	while (scanf ("%d %d", &n, &q) == 2){
+		memset (tree, 0, sizeof(tree));
+		memset (lazy, 0, sizeof(lazy));
+		build(0, 0, n-1);
+			
+		for(int i = 1; i <= q; i++){
+			
+			scanf(" %c %d %d", &op, &a, &b);
 			a--;
-			b--;
-			update_range(0, 0, n-1, a, b, k);
-		}else{
-			scanf("%d %d", &a, &b);
-			a--;
-			b--;
-			printf("%d\n", query(0, 0, n-1, a, b) );
-		}		
+			b--;	
+			
+			if( op == 'M'){
+				Node aux = query(0,0,n-1, a, b);
+				int maior = -1;
+				int indice = -1;
+				go(i,3){
+					if (maior < aux.v[i]){
+						maior = aux.v[i];
+						indice = i;
+					}
+				}
+				//printf("maior %d\n", maior);
+				if (maior > (b-a)/2)
+					update_range(0, 0, n-1, a, b, 1);
+			}else{
+				//scanf("%d %d", &a, &b);
+				Node aux = query(0,0,n-1, a,b);
+				printf("%d", aux.v[0]);
+				printf(" %d", aux.v[2]);
+				printf(" %d", aux.v[1]);
+				printf("\n");
+				//printf("%d\n", query(0, 0, n-1, a, b) );
+			}		
+		}
+		printf("\n");
 	}
-		
+	 	
 }
  
---
+++ spoj/MANUT.cpp
@@ -0,0 +1,99 @@
+/*
+* @Author: pedrotorres
+* @Date:   2018-04-24 19:29:47
+* @Last Modified by:   pedrotorres
+* @Last Modified time: 2018-09-06 09:27:30
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef vector < vector <int> > vvi;
+typedef vector <int>  vi;
+
+vvi g;
+vi menor, nivel, pai, art;
+int cont;
+
+set<int> arts_;
+
+void articulacao(int i){     
+	int nfilhos = 0;
+
+	nivel[i] = menor[i] = cont++;
+
+	for (int j = 0;j< g[i].size();j++){
+		int u = g[i][j];
+
+		if (nivel[u] == -1){
+			nfilhos++;
+			pai[u]=i;
+			articulacao(u);
+			menor[i] = min( menor[i], menor[u]);
+
+			if (nivel[i] != 1 && menor[u] >= nivel[i]) { 
+				arts_.insert(i);
+				art[i] = 1;
+			}
+
+		}
+		else if (pai[i] != u){
+			if(nivel[u] < menor[i]) menor[i] = nivel[u]; 
+		}                  
+	}
+
+	if (nivel[i] == 1 && nfilhos > 1) { 
+		arts_.insert(i);
+		art[i] = 1;
+	}
+}
+
+int main(){
+	int n,m;
+	int teste = 1;
+	cin >> n >> m;
+	
+	while(!(n == 0 && m == 0)){
+		g.resize(n + 1);
+		
+		for(int i = 0; i <= n; i++) g[i].clear();
+		
+		for(int i = 0; i < m; i++){
+			int a,b;
+			cin >> a >> b;
+			g[a].push_back(b);
+			g[b].push_back(a);
+		}
+		
+		cont = 1;
+		arts_.clear();
+		menor.assign(n + 1, -1);
+		pai.assign(n + 1, -1);
+		nivel.assign(n + 1, -1);
+		art.assign(n + 1,  0);
+		articulacao(1);
+
+		set<int>::iterator it;
+
+		printf("Teste %d\n" ,teste);
+
+		int arts_size = arts_.size();
+
+		if(arts_size){
+			it = arts_.begin();
+			for(int i = 0; i < arts_size - 1; i++){
+				printf("%d ", *it);
+				it++;
+			}		
+			printf("%d" ,*it);	
+		}
+		else{
+			printf("nenhum");
+		}
+		printf("\n\n");
+		scanf("%d %d" ,&n ,&m);
+		teste++;
+	}
+	
+}
